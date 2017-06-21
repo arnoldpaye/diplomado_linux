98fb4308f14581c8c3657b9863cd636c5f958796
Arnold Paye <arnoldpayet@gmail.com>
2017-06-21 10:17:08 -0400

    add test files.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,16 +7,26 @@
 require 'spec_helper'
 
 describe 'diplomado_linux::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
+    context 'when run on Centos 7.3.1611' do
+        let(:chef_run) do
+            runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
+            runner.converge(described_recipe)
+        end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+        it 'converges successfully' do
+            expect{ chef_run }.to_not raise_error
+        end
+
+        it 'installs httpd' do
+            expect(chef_run).to install_package 'httpd'
+        end
+
+        it 'enables the httpd service' do
+            expect(chef_run).to enable_service 'httpd'
+        end
+
+        it 'starts the httpd service' do
+            expect(chef_run).to start_service 'httpd'
+        end
     end
-  end
 end
